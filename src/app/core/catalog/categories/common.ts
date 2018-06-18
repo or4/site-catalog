6910eaa1b79e37944cd483b789f2912e902757b4
Dmitriy Vakarev <dvakarev@allmax.team>
2018-06-18 16:19:08 +0700

    bugfix, with name

--- src/app/core/catalog/categories/common.ts
+++ src/app/core/catalog/categories/common.ts
@@ -18,12 +18,12 @@ export const getCategoryCaption = (category: string, selectCategory: (category :
       if (isUndefined(acc.compositCategory)) {
         acc.compositCategory = categoryPart;
         const categorySelected = selectCategory(category);
-        if (categorySelected === null) { return acc }
+        if (!categorySelected) { return acc }
         acc.compositCaption = capitalizeFirstLetter(categorySelected.name.toLocaleLowerCase());
       } else {
         acc.compositCategory = `${acc.compositCategory}.${categoryPart}`;
         const categorySelected = selectCategory(category);
-        if (categorySelected === null) { return acc }
+        if (!categorySelected) { return acc }
         acc.compositCaption = `${acc.compositCaption}, ${categorySelected.name.toLocaleLowerCase()}`;
       }
       return acc;
