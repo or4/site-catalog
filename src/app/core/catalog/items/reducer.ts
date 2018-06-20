aecaf1b991537f11dd9fea9f01756c5ff10e3807
Dmitriy Vakarev <dvakarev@allmax.team>
2018-06-20 15:22:06 +0700

    selectItemsPage

--- src/app/core/catalog/items/reducer.ts
+++ src/app/core/catalog/items/reducer.ts
@@ -2,6 +2,7 @@ import { Reducer } from 'redux';
 
 import { ActionTypes, ActionsAll } from './actions';
 import { AppState } from 'store/reducers';
+import { log } from 'util/logger';
 
 export type TItem = {
   id: number;
@@ -35,7 +36,25 @@ export const itemsReducer: Reducer<TState> = (state: TState = initialState, acti
   }
 };
 
-export const selectItems =
+export const selectItems = (state: AppState) => state.items.data;
+
+export const selectItemsByCategory =
   (state: AppState, category: string) =>
-    state.items.data.filter(item => item.category.indexOf(category) >= 0);
+    selectItems(state).filter(item => item.category.indexOf(category) >= 0);
+
+// Counting pages from 1
+export const selectItemsPage =
+  (state: AppState, category: string, limit: number, page: number) => {
+    const all = selectItemsByCategory(state, category);
+
+    const output = {
+      data: all.slice((page - 1) * limit, page * limit),
+      limit,
+      page,
+      total: all.length,
+      totalPages: Math.ceil(all.length / limit),
+    };
+    log('selectItemsPage', all, output);
+    return output;
+  };
 
